54d77bcd6d40834f53dd44f5d1785b57f01cea72
Liter <hallgato@LABOR113-02>
2024-03-11 15:12:39 +0100

    FoodClass propertykel

---
+++ Workshop_Orai_4/Models/Food.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Orai_4.Models
+{
+    public class Food : ObservableObject
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); } 
+        }
+
+        private int price;
+
+        public int Price
+        {
+            get { return price; }
+            set { SetProperty(ref price, value); }
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { SetProperty(ref quantity, value); }
+        }
+    }
+}
